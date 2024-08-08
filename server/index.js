bad8cba6bfc91e7fc72f1ab3226d40d2ea6d2f28
Yuri Sanchez <yds123001@gmail.com>
2024-08-08 15:16:37 -0600

    added tailwind, removed starter files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+const clientId = "c05d183e90c64e4b97612a9b7616e9cc";
+const code = null;
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello World!" });
